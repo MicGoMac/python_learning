57bd0448d30a7920b3155991010c734c8065540c
michael <mic.go.mac@gmail.com>
2023-07-15 00:44:47 +0800

    modified:   hkpl_book/lesson01.py

--- hkpl_book/lesson01.py
+++ hkpl_book/lesson01.py
@@ -1,5 +1,15 @@
 #!/usr/bin/
 
 '''
-from book page 
+from book page 25 
 '''
+
+#variables. id() shows the memory location
+a=b=c=5
+id(a)
+id(b)
+id(c)
+#above gives same result
+a=6
+id(a)
+#result diff, as a now using another memory location
