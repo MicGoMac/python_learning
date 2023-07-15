078aa70eb1dca48d9c5c784147c3b2be19704839
michael <mic.go.mac@gmail.com>
2023-07-15 14:51:07 +0800

    added datatype in hkpl_book/lesson01.py

--- hkpl_book/lesson01.py
+++ hkpl_book/lesson01.py
@@ -1,10 +1,10 @@
 #!/usr/bin/
 
 '''
-from book page 25 
+from book chapter 2, page 25 
 '''
 
-#variables. id() shows the memory location
+#1. variables. id() shows the memory location
 a=b=c=5
 id(a)
 id(b)
@@ -14,7 +14,19 @@ a=6
 id(a)
 #result diff, as a now using another memory location
 
-#keywords cannot be used as var name. keyword list is available like this:
+#2.keywords cannot be used as var name. keyword list is available like this:
 import keyword
 print(keyword.kwlist)
 #['False', 'None', 'True', '__peg_parser__', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+
+
+#3. datatypes : str, int, float, bool, none
+#get the type of a var:
+price=3.4
+print( type(price))
+#or
+print( "price is ", type(price))
+
+#mutable and immutable datatype(p.27)
+
+
