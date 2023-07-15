3fe0b6d2696e192e5eb705200dc1ed68db585b02
michael <mic.go.mac@gmail.com>
2023-07-15 14:07:32 +0800

    updated with keyword in hkpl_book/lesson01.py

--- hkpl_book/lesson01.py
+++ hkpl_book/lesson01.py
@@ -13,3 +13,8 @@ id(c)
 a=6
 id(a)
 #result diff, as a now using another memory location
+
+#keywords cannot be used as var name. keyword list is available like this:
+import keyword
+print(keyword.kwlist)
+#['False', 'None', 'True', '__peg_parser__', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
