0fc030b59f86cdf4b700de4f9a0ae73d3852576a
michael <mic.go.mac@gmail.com>
2023-07-19 18:07:26 +0800

    added series of baisc/sample dir in kivy

---
+++ kivy/basic_dir01/hello.py
@@ -0,0 +1,11 @@
+
+BoxLayout:
+    orientation:"vertical"
+    Button:
+        text:"Botton1"
+
+    Button:
+        text:"Botton2"
+
+    Button
+        text:"Button3"
---
+++ kivy/basic_dir01/main.py
@@ -0,0 +1,14 @@
+import kivy
+from kivy.app import App
+
+from kivy.uix.button import Button
+
+kivy.require("1.0.9")
+
+class HelloApp(App):
+    #pass to kv file?
+    pass
+
+if __name__=="__main__":
+    HelloApp().run
+
