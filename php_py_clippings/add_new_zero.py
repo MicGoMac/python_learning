d770bfb6aa1637fa56da2e268f7fb996b4dfb65a
michael <mic.go.mac@gmail.com>
2023-07-29 16:09:06 +0800

    rev a few

--- php_py_clippings/add_new_zero.py
+++ php_py_clippings/add_new_zero.py
@@ -1,12 +1,12 @@
 ﻿#this for strings
-def addzero_2_str( s, digits ):
-	return s.zfill(digits)
+def addzero_2_str( string, digits ):
+	return string.zfill(digits)
 
 #this for int
 def add_zero( int, digits ): 
-	para = "%0"+digits+"d";
+	para = "%0"+ str(digits) +"d";
 	return para % int 
 
-print( addzero_2_str('32ia',6) )
+#print( addzero_2_str('32ia',6) )
 
-print( add_zero(32,"6"))
+#print( add_zero(32,"6"))
