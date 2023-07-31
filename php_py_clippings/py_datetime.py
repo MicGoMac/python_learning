40ae0e4706564e0df2be6f3fbcfb5870f8224149
michael <mic.go.mac@gmail.com>
2023-07-31 21:30:53 +0800

    php_py_clippings/py_datetime.py under construction 1

--- php_py_clippings/py_datetime.py
+++ php_py_clippings/py_datetime.py
@@ -12,9 +12,21 @@ x = datetime.datetime(2020, 5, 17, 18, 19, 20, 8)
 #this not working:
 print(x.timezone)
 
+#day of month
+print(x.strftime("%d"))
+
 x = datetime.datetime.now()
 print(x)
 print(x.year)
+
+datetime_code={ "short_weekday":%a, "weekday":%A,"weekday_num":%w, "0day_num":%d,"day_num":%-d,  
+ "short_month":%b,"0month_num":%m,"month_num":%-m,"month":%B,  "year":%Y,"0short_year":%y,"short-yearw":%-y,
+"0hour":%H,"hour":%-H,"012hour":%I,"12hour":%-I,"ampm":%p,
+"0min":%M,"min":%-M,"0sec":%S, "sec":%-S, "microsec":%f,
+"utc":%z,"timezone":%Z,"0dayofyear":%j,"dayofyear":%-j,
+"week_num":%U,"0week_num":%W,  "locale_datetime":%c,"locale_date":%x,"locale_time":%X, "%":%%, 
+}
+
 print(x.strftime("%A"))
 
 
