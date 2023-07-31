c2fdee2a1b1f2830328fb1198a12c452df8b357b
michael <mic.go.mac@gmail.com>
2023-07-31 22:26:41 +0800

    php_py_clippings/py_datetime.py under construction 2

--- php_py_clippings/py_datetime.py
+++ php_py_clippings/py_datetime.py
@@ -2,9 +2,11 @@
 
 import datetime
 
+
 #The datetime() class requires three parameters to create a date: year, month, day.
 #also takes parameters for time and timezone (hour, minute, second, microsecond, tzone), but they are optional,
 
+#datetime is an obj inside datetime, so the datetime.datetime
 x = datetime.datetime(2020, 5, 17)
 print(x)
 
@@ -13,21 +15,21 @@ x = datetime.datetime(2020, 5, 17, 18, 19, 20, 8)
 print(x.timezone)
 
 #day of month
-print(x.strftime("%d"))
+print(x.datetime.strftime("%d"))
 
 x = datetime.datetime.now()
 print(x)
 print(x.year)
 
-datetime_code={ "short_weekday":%a, "weekday":%A,"weekday_num":%w, "0day_num":%d,"day_num":%-d,  
- "short_month":%b,"0month_num":%m,"month_num":%-m,"month":%B,  "year":%Y,"0short_year":%y,"short-yearw":%-y,
-"0hour":%H,"hour":%-H,"012hour":%I,"12hour":%-I,"ampm":%p,
-"0min":%M,"min":%-M,"0sec":%S, "sec":%-S, "microsec":%f,
-"utc":%z,"timezone":%Z,"0dayofyear":%j,"dayofyear":%-j,
-"week_num":%U,"0week_num":%W,  "locale_datetime":%c,"locale_date":%x,"locale_time":%X, "%":%%, 
+datetime_code={ "short_weekday":"%a", "weekday":"%A","weekday_num":"%w", "0day_num":"%d","day_num":"%-d",  
+ "short_month":"%b","0month_num":"%m","month_num":"%-m","month":"%B",  "year":"%Y","0short_year":"%y","short-yearw":"%-y",
+"0hour":"%H","hour":"%-H","012hour":"%I","12hour":"%-I","ampm":"%p",
+"0min":"%M","min":"%-M","0sec":"%S", "sec":"%-S", "microsec":"%f",
+"utc":"%z","timezone":"%Z","0dayofyear":"%j","dayofyear":"%-j",
+"week_num":"%U","0week_num":"%W",  "locale_datetime":"%c","locale_date":"%x","locale_time":"%X", "%":"%%", 
 }
 
-print(x.strftime("%A"))
+print(x.datetime.strftime( datetime_code["min"]))
 
 
 
