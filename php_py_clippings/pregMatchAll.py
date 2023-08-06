86052039eaecaa4eea9e7a44a8770d42d8cd73a0
michael <mic.go.mac@gmail.com>
2023-08-06 15:22:11 +0800

    updated php_py_clippings/pregMatchAll.py with re module

--- php_py_clippings/pregMatchAll.py
+++ php_py_clippings/pregMatchAll.py
@@ -1,1 +1,26 @@
-﻿ $pattern='/watch\?v=.(.*)class/';            preg_match_all( $pattern, $data, $matches);                if ($matches[0]!= array()) {                                                       }
+import re
+
+pattern="quick.*fox"
+
+t="The quick brown fox jumps over the lazy dog The quick brown fox jumps over the lazy dog"
+
+res = (re.search(pattern, t))
+#res = None if no matched
+print("search returns obj")
+print(res)
+print(res.span())
+print(res.group())
+print(res.string)
+
+
+print("\nfindall")
+res = (re.findall(pattern, t))
+print(res)
+
+print("\nsplit")
+res = (re.split(pattern, t))
+print(res)
+
+print("\nreplace")
+res = (re.sub(pattern, "The cat", t))
+print(res)
