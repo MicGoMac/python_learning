cf0e99e88fe2a940f14abb2db58be170d4ae42e7
michael <mic.go.mac@gmail.com>
2023-08-09 01:13:53 +0800

    updated date_past_day.py

--- php_py_clippings/date_past_day.py
+++ php_py_clippings/date_past_day.py
@@ -1,1 +1,10 @@
-﻿date_default_timezone_set('Asia/Hong_Kong'); $today = new DateTime();  date_sub($today, date_interval_create_from_date_string('10 days'));echo date_format($today, 'Y-m-d');
+﻿
+
+
+
+'''
+date_default_timezone_set('Asia/Hong_Kong'); 
+$today = new DateTime();  
+date_sub($today, date_interval_create_from_date_string('10 days'));
+echo date_format($today, 'Y-m-d');
+'''
