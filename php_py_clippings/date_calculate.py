f9a4b87229320092978a94d1908fff69354d1a9c
michael <mic.go.mac@gmail.com>
2023-08-09 01:08:32 +0800

    half done dateCheck and date_calculate

--- php_py_clippings/date_calculate.py
+++ php_py_clippings/date_calculate.py
@@ -1,1 +1,16 @@
-﻿function past_date($date, $days) {    $t1 = strtotime($date);    $t2 = $t1 - $days*3600*24;    return date("Y-m-d", $t2);}
+﻿
+#days can be +ve -ve
+def add_days( dt_obj, days ):
+    timestamp_orig = 
+    seconds_diff = timestamp_orig + days*3600*24
+    
+    #convert back to dt obj
+
+
+'''
+function past_date($date, $days) {
+    $t1 = strtotime($date);
+    $t2 = $t1 - $days*3600*24;
+    return date("Y-m-d", $t2);
+}
+'''
