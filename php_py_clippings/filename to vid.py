f76be1472b6a4b58818d8b1e83a51fb8b867aae6
michael <mic.go.mac@gmail.com>
2023-08-13 17:14:39 +0800

    rm some non python

--- php_py_clippings/filename to vid.py
+++
@@ -1,6 +0,0 @@
-﻿def filename_to_vid( f ):
-	pi = pathinfo(f)
-	
-	ext= pi['extension']
-	fn= pi['filename']
-	return substr( fn, -11)
--- php_py_clippings/test.py
+++ php_py_clippings/test.py
@@ -1,5 +1,33 @@
-t="""abc
-def
+import sys
+
+print("Argument List:", str(sys.argv))
+
+
+
+def test1():
+	t="""abc
 ghi"""
 
-print(t)
+	print(t)
+
+
+def test2():
+	print ("this is test2")
+
+got_argv = sys.argv[1]
+
+print(got_argv)
+exit()
+
+'''
+#this need python 10 or above
+match got_argv:
+	case "test1":
+		test1()
+
+	case "test2":
+		test2()
+
+	case _:
+		print ( "Pls give a param which is the name of the function" )
+'''
