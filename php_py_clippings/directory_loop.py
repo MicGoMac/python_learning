a7c2e289b8604cc675609948558fd88bd5d59bc3
michael <mic.go.mac@gmail.com>
2023-08-25 10:58:30 +0800

    some clean up

--- php_py_clippings/directory_loop.py
+++ php_py_clippings/directory_loop.py
@@ -1,1 +1,22 @@
-﻿function checkDir ( $ddir ) { //works on FILES in a given Directory. Skip any Dir inside $dirHandle = opendir( $ddir);  while($file=readdir($dirHandle)){        if( is_dir($file) || substr($file, 0,1) == '.' ){           // readDirs($main.$file); // no need to go sub folders           echo "skip".$file."\n";           continue; // exit repeat      }      else{       //files. where work start       $path_info = pathinfo($ddir.$file);          // optiional check // if ( strlen($file) > 19 && ($path_info['extension']=='htm' || $path_info['extension']=='html' ) ) {                      }       }  }
+﻿
+
+
+'''
+function checkDir ( $ddir ) {
+ //works on FILES in a given Directory. Skip any Dir inside
+ $dirHandle = opendir( $ddir); 
+ while($file=readdir($dirHandle)){  
+      if( is_dir($file) || substr($file, 0,1) == '.' ){ 
+          // readDirs($main.$file); // no need to go sub folders 
+          echo "skip".$file."\n"; 
+          continue; // exit repeat
+      }
+      else{
+       //files. where work start
+       $path_info = pathinfo($ddir.$file);
+          // optiional check // if ( strlen($file) > 19 && ($path_info['extension']=='htm' || $path_info['extension']=='html' ) ) { 
+             
+        } 
+      }
+  }
+'''
