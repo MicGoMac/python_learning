b98262c8c51368cacd66383b24a5e056408be7ba
michael <michael@michaels-Mac-mini.local>
2023-09-19 08:09:21 +0800

    added git from learn-more-python-the-hard-way-solutions book sample

--- php_py_clippings/bom.py
+++ php_py_clippings/bom.py
@@ -1,5 +1,9 @@
 ﻿
 #ref to read:https://stackoverflow.com/questions/39094125/python-can-i-add-utf8-bom-to-a-file-without-opening-it
-bom=(""   )
+bom=(b"0xEF" )
+
+#print(bom
+
+print(bom.encode("UTF-8"))
 
 #$bom =( chr(0xEF) . chr(0xBB) . chr(0xBF) ); 
